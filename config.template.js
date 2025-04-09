498fb10610a955a322dc5a2366ad484eb6a715b0
SiymX <khsayeem17@gmail.com>
2025-04-08 21:09:18 -0400

    Update config.template.js placeholder

--- config.template.js
+++ config.template.js
@@ -1,4 +1,4 @@
 // config.template.js
 // Copy this file to config.js and insert your API key
-export const OPENROUTER_KEY = "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";
+export const OPENROUTER_KEY = "your-api-key-here";
 
